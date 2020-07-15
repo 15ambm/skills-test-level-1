232a9989f48b1bbad16bb2f0ea0521e4d875ddaf
Alex <15ambm@queensu.ca>
2020-07-15 13:58:48 -0400

    implemented GET /public/getGladiators/:year endpoint

--- src/controllers/public.js
+++ src/controllers/public.js
@@ -1,8 +1,53 @@
 
+const fs = require('fs')
+const csv = require('csv-parser');
+
 function getGladiatorsByYear(req, res) {
+
+  let data = []
   const year = req.params.year;
-  console.log(year)
-  res.json("")
+  
+  fs.createReadStream("gladiators.csv")
+    .pipe(csv())
+    .on('data', (row) => {
+      data.push(row)
+    })
+    .on('end', () => {
+      if(validateYear(year)) {
+        sendResponse(data)
+      } else {
+        res.json([])
+      }
+    })
+
+    const validateYear = (year) => {
+      let parsedYear = parseInt(year, 10)
+      if(isNaN(parsedYear)) {
+        console.log("Invalid Year")
+        return false
+      } else return true
+    }
+
+    const sendResponse = (data) => {
+      let response = filterGladiators(data)
+      console.log(`Retrieved ${response.length} gladiators in the year ${year}`)
+      if(response.length > 0) {
+        res.json(response)
+      } else {
+        res.json([])
+      }
+    }
+    
+    const filterGladiators = (data) => {
+      const response = data.filter(item => {
+        let firstYear = parseInt(item['first year'], 10)
+        let lastYear = parseInt(item['last year'], 10)
+        if(year >= firstYear && year <= lastYear) {
+          return true
+        }
+      })
+      return response
+    }
 }
 
 module.exports = {
